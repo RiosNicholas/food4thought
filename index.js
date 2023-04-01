bbc812ff34306d672b6aedb66e8467b352ae4076
Nicholas Rios <rios.nicholas@protonmail.com>
2023-04-01 16:18:57 -0400

    Update theme-button capabilities - Update button text to reflect what mode clicking would toggle to - Add dark mode to nav and footer - Change the anchor link color properties in dark-mode using the "gruvbox" color palette - Change color of nav header in dark mode to a shade of green in the "gruvbox" color palette

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
+/* DARK MODE */
 // Query for button with an id "theme-button"
 let themeButton = document.getElementById("theme-button");
+let navContainer = document.querySelector("nav");
+let footerContainer = document.querySelector("footer");
 
 const toggleDarkMode = () => {
     // const sections = document.querySelectorAll("section");
     // sections.forEach(section => section.classList.toggle("dark-mode"));
     document.body.classList.toggle("dark-mode");
+    document.querySelector("nav").classList.toggle("dark-mode-nav-footer");
+    document.querySelector("footer").classList.toggle("dark-mode-nav-footer");
+    
+    
+    if (document.body.classList.contains("dark-mode")) {
+        themeButton.innerHTML = "Toggle Light Mode";
+    } else {
+        themeButton.innerHTML = "Toggle Dark Mode";
+    }
 }
-
-
 // Registers a 'click' event listener for the theme button with toggleDarkMode as the callback function.
 themeButton.addEventListener("click", toggleDarkMode);
+
+
+/* PETITION */
+const addSignature = () => {
+
+}
